60b7a1f7cb058d97333c60566d7a489d6ea4fcab
Mahi <starexify@gmail.com>
2022-05-22 20:35:10 +1000

    Starter code for projects
    Just mapping each project name to a h3 tag

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-// import projects from '../data/projects.json'
+import global from '../css/Global.module.css'
+import projects from '../data/projects.json'
 
 function Projects() {
   return (
     <section>
-      {/* {projects.map(project => (
-        <h1>{project.title}</h1>
-      ))} */}
+      <div className={global.container}>
+        <h2 className={global.secondary_text}>Projects</h2>
+        {projects.map(project => (
+          <h3>{project.title}</h3>
+        ))}
+      </div>
     </section>
   )
 }
