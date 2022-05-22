bc85c0584462f9d1e4713def46ce750e3c1790a0
Mahi <starexify@gmail.com>
2022-05-22 23:20:41 +1000

    Buttons + Tags for project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,8 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { Chip } from '@mui/material';
+import Button from '@mui/material/Button';
 import global from '../css/Global.module.css'
 import style from '../css/Projects.module.css'
 import projects from '../data/projects.json'
@@ -16,7 +17,6 @@ function Projects() {
         <div className={style.project_container}>
           {projects.map((project, index) => (
             <Card className={style.card_color} key={index} sx={{ maxWidth: 345 }}>
-            <CardActionArea>
               <CardMedia
                 className={style.project_img_size}
                 component='img'
@@ -30,8 +30,16 @@ function Projects() {
                 <Typography className={style.card_description}>
                   {project.description}
                 </Typography>
+                <div className={style.btn_container}>
+                  {project.url && <Button variant="contained" onClick={() => window.open(project.url, '_blank').focus()}>URL</Button>}
+                  <Button variant="contained" onClick={() => window.open(project.repository, '_blank').focus()}>Git Repository</Button>
+                </div>
+                <div className={style.tags_container}>
+                  {project.stack.map((tag, index) => (
+                    <Chip key={index} className={style.tags} label={tag}/>
+                  ))}
+                </div>
               </CardContent>
-            </CardActionArea>
           </Card>
           ))}
         </div>
